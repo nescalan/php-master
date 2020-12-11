315db74f434bc2bc9010b4bea9cafdc731ee5619
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2020-12-10 22:24:05 -0600

    2: FizzBuzz Mod 3

---
+++ Eloquent JavaScript/Cap 2/FizzBuzz.js
@@ -0,0 +1,11 @@
+let number;
+
+function FizzBuzz() {
+  for (i = 1; i <= 100; i++) {
+    if (i % 3) {
+      console.log("el numero " + i + " no es divisible entre tres.");
+    } else {
+      console.log(i);
+    }
+  }
+}
