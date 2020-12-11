775ad295b4efec476a203b4c1f6ca5a334124de0
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2020-12-10 21:54:50 -0600

    1: Triangle Done

---
+++ Eloquent JavaScript/Cap 1/1 Triangle/1-LoopingTriangle.js
@@ -0,0 +1,8 @@
+// FIRST EXCERCISE
+let triangle = ["#", "##", "###", "####", "#####", "######", "#######"];
+
+function MakeTriangle() {
+  for (let i = 0; i < triangle.length; i++) {
+    console.log(triangle[i]);
+  }
+}
--- Eloquent JavaScript/Cap1-Functions/functions.js
+++
@@ -1,6 +0,0 @@
-// FIRST EXCERCISE
-let counter;
-
-for (let i = 0; i < 7; i++) {
-  console.log("funciona");
-}
