e8eb15265ba052d00504f45c9b30e4bc5f60d653
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2020-12-23 13:36:41 -0600

    Cap 1: Hot Module Replacement

--- PracticasReactApp/primerEjercicio/hackernews/src/App.js
+++ PracticasReactApp/primerEjercicio/hackernews/src/App.js
@@ -8,6 +8,9 @@ function App() {
     <div className="App">
       <h1> {helloWorld} </h1>
       <h2> {secondMessage} </h2>
+      <h3>Today is {new Date().toLocaleDateString()}.</h3>
+      <h3>The Time is: {new Date().toLocaleTimeString()}.</h3>
+      <p>Well is going to be hard, but I think it's a great oportunity</p>
     </div>
   );
 }
--- PracticasReactApp/primerEjercicio/hackernews/src/index.js
+++ PracticasReactApp/primerEjercicio/hackernews/src/index.js
@@ -4,17 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
+
+if (module.hot) {
+  module.hot.accept();
 }
 
-setInterval(tick, 1000);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
