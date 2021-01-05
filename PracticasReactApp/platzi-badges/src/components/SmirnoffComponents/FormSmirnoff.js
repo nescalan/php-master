94ec9a3e073c036c0535d06e156f306eae900623
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2021-01-05 16:56:30 -0600

    Smirnoff: State Lift Up

--- PracticasReactApp/platzi-badges/src/components/SmirnoffComponents/FormSmirnoff.js
+++ PracticasReactApp/platzi-badges/src/components/SmirnoffComponents/FormSmirnoff.js
@@ -1,38 +1,41 @@
 import React, { Component } from "react";
 
 class FormSmirnoff extends Component {
-  state = {};
-  handleOnChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
+  // state = {};
 
-  handleOnClick = (event) => {
+  // handleOnChange = (event) => {
+  //   this.setState({
+  //     [event.target.name]: event.target.value,
+  //   });
+  // };
+
+  handleOnClick = (e) => {
     console.log("button was clicked");
-    console.log(this.state);
   };
 
-  handleOnSubmit = (event) => {
-    event.preventDefault();
+  handleOnSubmit = (e) => {
+    e.preventDefault();
     console.log("Form was submited");
+    console.log(this.state);
   };
 
   render() {
     return (
       <div>
+        <h1>New Form</h1>
+
         <form onSubmit={this.handleOnSubmit}>
           <div className="form-group">
             <label htmlFor="exampleInputEmail1">Email address</label>
             <input
-              onChange={this.handleOnChange}
+              onChange={this.props.onChange}
               type="email"
               className="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
               placeholder="Enter email"
-              name="EmailAddress"
-              value={this.state.EmailAddress}
+              name="emailAddress"
+              value={this.props.formValues.emailAddress}
             />
             <small id="emailHelp" className="form-text text-muted">
               We'll never share your email with anyone else.
@@ -41,21 +44,23 @@ class FormSmirnoff extends Component {
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Password</label>
             <input
-              onChange={this.handleOnChange}
+              onChange={this.props.onChange}
               type="password"
               className="form-control"
               id="exampleInputPassword1"
               placeholder="Password"
-              name="Password"
-              value={this.state.Password}
+              name="password"
+              value={this.props.formValues.password}
             />
           </div>
           <div className="form-group form-check">
             <input
-              onChange={this.handleOnChange}
+              onChange={this.props.onChange}
               type="checkbox"
               className="form-check-input"
               id="exampleCheck1"
+              name="checkbox"
+              value={this.props.formValues.checkbox}
             />
             <label className="form-check-label" htmlFor="exampleCheck1">
               Check me out
--- PracticasReactApp/platzi-badges/src/pages/SmirnoffPages/SmirnoffNew.js
+++ PracticasReactApp/platzi-badges/src/pages/SmirnoffPages/SmirnoffNew.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import NavbarSmirnoff from "../../components/SmirnoffComponents/NavbarSmirnoff";
 import HeroSmirnoff from "../../components/SmirnoffComponents/SmirnofHero";
@@ -7,28 +7,42 @@ import FormSmirnoff from "../../components/SmirnoffComponents/FormSmirnoff";
 
 import "./SmirnoffNew.css";
 
-const SmirnoffNew = () => {
-  return (
-    <div>
-      <NavbarSmirnoff />
-      <HeroSmirnoff />
+class SmirnoffNew extends Component {
+  state = { form: {} };
 
-      <div className="container">
-        <div className="row">
-          <div className="col-6">
-            <BadgeSmirnoff
-              title="Smirnoff No. 21 Vodka"
-              firstPrice="750ml bottle - from $14.99 "
-              secondPrice="1.750l plastic bottle - from $22.99 "
-            />
-          </div>
-          <div className="col-4">
-            <FormSmirnoff />
+  handleChange = (e) => {
+    this.setState({
+      ...this.state.form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <NavbarSmirnoff />
+        <HeroSmirnoff />
+
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <BadgeSmirnoff
+                title="Smirnoff No. 21 Vodka"
+                firstPrice="750ml bottle - from $14.99 "
+                secondPrice="1.750l plastic bottle - from $22.99 "
+              />
+            </div>
+            <div className="col-4">
+              <FormSmirnoff
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default SmirnoffNew;
