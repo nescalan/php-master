bbf3dce764689fd7391c3d335be521b479c3c620
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2021-01-05 20:14:09 -0600

    17: Badges List, end of state

---
+++ PracticasReactApp/platzi-badges/src/components/BadgesList.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class BadgesList extends Component {
+  state = {};
+  render() {
+    return (
+      <ul className="list-unstyled">
+        {this.props.badges.map((badge) => {
+          return (
+            <li key={badge.id}>
+              <p>
+                {badge.firstName} {badge.lastName}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default BadgesList;
--- PracticasReactApp/platzi-badges/src/index.js
+++ PracticasReactApp/platzi-badges/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// import BadgeNew from "./pages/BadgeNew";
-import SmirnoffNew from "./pages/SmirnoffPages/SmirnoffNew";
+import BadgeNew from "./pages/BadgeNew";
+import Badges from "./pages/Badges";
+// import SmirnoffNew from "./pages/SmirnoffPages/SmirnoffNew";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 
-// ReactDOM.render(<BadgeNew />, document.getElementById("root"));
-ReactDOM.render(<SmirnoffNew />, document.getElementById("root"));
+ReactDOM.render(<Badges />, document.getElementById("root"));
+// ReactDOM.render(<SmirnoffNew />, document.getElementById("root"));
