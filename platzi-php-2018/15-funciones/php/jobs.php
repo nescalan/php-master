50f676cbe24ad7dbc3c4034576ce7b7b6d7ff446
Ing. Nelson Gonzalez <nescalan@gmail.com>
2021-06-02 16:03:12 -0600

    Programacion Orientada a Objetos

---
+++ platzi-php-2018/15-funciones/php/jobs.php
@@ -0,0 +1,35 @@
+<?php
+$name = 'Hector Benitez';
+$limitMonths = 2000;
+$jobs = [
+  [
+    'title' => 'PHP Developer',
+    'description' => 'This is an awesome job!!!',
+    'visible' => true,
+    'months' => 16
+  ],
+  [
+    'title' => 'Python Dev',
+    'visible' => false,
+    'months' => 14
+  ],
+  [
+    'title' => 'Devops',
+    'visible' => true,
+    'months' => 5
+  ],
+  [
+    'title' => 'Node Dev',
+    'visible' => true,
+    'months' => 24
+  ],
+  [
+    'title' => 'Frontend Dev',
+    'visible' => true,
+    'months' => 3
+  ]
+];
+
+
+
+?>
