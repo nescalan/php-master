7738a1249f9f9507463f374b3d8aceb692664c3f
Ing. Nelson Gonzalez <nescalan@gmail.com>
2021-06-07 19:05:12 -0600

    14: Condicionales

---
+++ falcon-masters/Seccion 2/14_condicional-if.php
@@ -0,0 +1,22 @@
+<?php
+
+$edad = 19;
+
+if($edad >= 18) {
+    echo '<h1> Eres mayor de edad </h1>';
+} else {
+    echo '<h1> Eres menor de edad </h1>';
+}
+
+ if($edad >= 18){ ?>
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Sitio</title>
+	</head>
+	<body>
+		<h1>Bienvenido!</h1>
+	</body>
+	</html>
+<?php } ?>
