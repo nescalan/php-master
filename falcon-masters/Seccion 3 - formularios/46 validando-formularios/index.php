aeaa1208e39530ed503bf09ee68c652a553857e3
Ing. Nelson Gonzalez <nescalan@gmail.com>
2021-06-15 13:41:53 -0600

    46: Primera parte Formularios

---
+++ falcon-masters/Seccion 3 - formularios/46 validando-formularios/index.php
@@ -0,0 +1,40 @@
+<?php
+
+$errores = '';
+
+if (isset($_POST['submit'])) {
+    $nombre =$_POST['nombre'];
+    $correo =$_POST['correo'];
+
+        if (!empty($nombre)) {
+            // $nombre = trim($nombre);
+            // $nombre = htmlspecialchars($nombre);    
+            // $nombre = stripslashes($nombre);
+            $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+            echo 'Tu nombre es: ' . $nombre . ' y tu correo es: ' . $correo;
+        } else {
+            $errores .= 'Por favor, agregar un nombre ';
+        }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>46 Validacion de Formulario</title>
+</head>
+<body>
+    <h1>46 Validacion de Formulario</h1>
+
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+        <input type="text" name="nombre" placeholder="Nombre: ">
+        <input type="email" name="correo" id="" placeholder="Correo: ">
+
+        <input type="submit" value="enviar" name="submit">
+    </form>
+</body>
+</html>
