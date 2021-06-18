ea231381fa47bb80f3b6685be7798f70157dedb9
Ing. Nelson Gonzalez <nescalan@gmail.com>
2021-06-18 11:15:12 -0600

    practica formularios

--- falcon-masters/Seccion 4 - practica formularios/48 Practica-Formularios/formulario-contacto/index.view.php
+++ falcon-masters/Seccion 4 - practica formularios/48 Practica-Formularios/formulario-contacto/index.view.php
@@ -11,7 +11,7 @@
 </head>
 <body>
     <div class="wrap">
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
         <input type="text" class="form-control" name="nombre" id="nombre"  placeholder="Nombre: " value="">
         <input type="text" class="form-control" name="correo" id="correo"  placeholder="Correo: " value="">
         <textarea name="mensaje" class="form-control" id="mensaje" placeholder="Mensaje"></textarea>
